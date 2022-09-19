3b2b7513d9d5f66c1164aac35b9c650380a6d9a9
efratah <natashaccsales18@gmail.com>
2022-09-20 06:47:06 +1200

    comments section

---
+++ comments/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+
+function App() {
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
+
+  const onClickHandler = () => {
+    setComments((comments) => [...comments, ...comments]);
+  };
+
+  const onChangeHandler = (e) => {
+    setComment(e.target.value);
+  };
+  
+  return (
+    <div className="main-container">
+      {comments.map((text) => (
+        <div className="comment-container"> {text} </div>
+      ))}
+
+      <div className="comment-flexbox">
+        <h3 className="comment-text">Add comment</h3>
+        <textarea
+          value={comment}
+          onChange={onChangeHandler}
+          className="input-box"
+        />
+        <button onClick={onClickHandler} className="comment-button">
+          {' '}
+          Submit{' '}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
